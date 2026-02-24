7bf24c2c3f69e44ad839d718de7cd48a9c19efb7
Dmytro Shulha <dimonchik0218@gmail.com>
2026-02-24 12:07:32 +0200

    delivery_drone_done

--- app/main.py
+++ app/main.py
@@ -2,4 +2,86 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list | None = None
+    ) -> None:
+        if coords is None:
+            coords = [0, 0]
+        self.name = name
+        self.weight = weight
+        self.coords = coords
+
+    def go_forward(self, step: int = 1) -> None:
+        if len(self.coords) < 2:
+            raise ValueError("coords must contain at least x and y")
+        self.coords[1] += step
+
+    def go_back(self, step: int = 1) -> None:
+        if len(self.coords) < 2:
+            raise ValueError("coords must contain at least x and y")
+        self.coords[1] -= step
+
+    def go_right(self, step: int = 1) -> None:
+        if len(self.coords) < 2:
+            raise ValueError("coords must contain at least x and y")
+        self.coords[0] += step
+
+    def go_left(self, step: int = 1) -> None:
+        if len(self.coords) < 2:
+            raise ValueError("coords must contain at least x and y")
+        self.coords[0] -= step
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list | None = None
+    ) -> None:
+        if coords is None:
+            coords = [0, 0, 0]
+        super().__init__(name, weight, coords)
+
+    def go_up(self, step: int = 1) -> None:
+        if len(self.coords) < 3:
+            raise ValueError("coords must contain z")
+        self.coords[2] += step
+
+    def go_down(self, step: int = 1) -> None:
+        if len(self.coords) < 3:
+            raise ValueError("coords must contain z")
+        self.coords[2] -= step
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list | None = None,
+            max_load_weight: int = 0,
+            current_load: Cargo | None = None
+    ) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = None
+
+        if current_load is not None:
+            self.hook_load(current_load)
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+        return
+
+    def unhook_load(self) -> None:
+        self.current_load = None
